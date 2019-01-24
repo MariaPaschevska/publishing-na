f9f9db23c86fba4b7fa7e3158a06eafe41377813
MariaPaschevska <maria.paschevska@gmail.com>
2019-01-24 12:01:03 +0200

    basic routing organized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from "./about/about.component";
+import {BookEditComponent} from "./books/book-edit/book-edit.component";
+import {BookDetailsComponent} from "./books/book-details/book-details.component";
+import {ProjectsComponent} from "./projects/projects/projects.component";
+import {BooksComponent} from "./books/books/books.component";
+import {ContactsComponent} from "./contacts/contacts.component";
+import {ProjectDetailsComponent} from "./projects/project-details/project-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'books', component: BooksComponent,
+    children: [
+      { path: ':id', component: BookDetailsComponent },
+      { path: ':id/edit', component: BookEditComponent }
+    ]
+  },
+  { path: 'projects', component: ProjectsComponent,
+    children: [
+      { path: ':id', component: ProjectDetailsComponent }
+    ]
+  },
+  { path: 'contacts', component: ContactsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
