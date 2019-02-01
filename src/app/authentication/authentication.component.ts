fc35a09c89941477ece1086cb20777da9195b1e1
MariaPaschevska <maria.paschevska@gmail.com>
2019-02-01 16:34:10 +0200

    Authentication form added

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BsModalRef} from "ngx-bootstrap";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-authentication',
@@ -7,6 +8,11 @@ import {BsModalRef} from "ngx-bootstrap";
   styleUrls: ['./authentication.component.sass']
 })
 export class AuthenticationComponent implements OnInit {
+
+  authForm = new FormGroup({
+    login: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
 
   constructor(
     public modalRef: BsModalRef
