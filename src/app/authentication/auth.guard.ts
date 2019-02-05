08490c741b82c132fb0d6adf757ab3db50678a1b
MariaPaschevska <maria.paschevska@gmail.com>
2019-02-05 13:16:03 +0200

    AuthGuard implemented

--- src/app/authentication/auth.guard.ts
+++ src/app/authentication/auth.guard.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthenticationService} from "../services/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot): boolean {
+    return this.checkAdmin();
+  }
+
+  checkAdmin() {
+    if (this.authService.checkAdmin()) {
+      return true;
+    }
+
+    this.router.navigate(['/books']);
+    return false;
   }
 }
