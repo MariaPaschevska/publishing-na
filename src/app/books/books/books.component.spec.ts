2953d5b96b530b7b5ac6c0417b5d58d1d855bff3
MariaPaschevska <maria.paschevska@gmail.com>
2019-02-14 11:25:18 +0200

    Book Component initial Test fixed

--- src/app/books/books/books.component.spec.ts
+++ src/app/books/books/books.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BooksComponent } from './books.component';
+import {ActivatedRoute, RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterTestingModule} from "@angular/router/testing";
+import {of} from "rxjs";
 
-describe('BooksComponent', () => {
+fdescribe('BooksComponent', () => {
   let component: BooksComponent;
   let fixture: ComponentFixture<BooksComponent>;
 
+  const fakeActivatedRoute = {
+    url: of({})
+  } as ActivatedRoute;
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BooksComponent ]
+      declarations: [ BooksComponent ],
+      imports: [ RouterModule, HttpClientModule, RouterTestingModule ],
+      providers: [ {provide: ActivatedRoute, useValue: fakeActivatedRoute} ]
     })
     .compileComponents();
   }));
