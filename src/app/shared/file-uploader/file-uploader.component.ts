0264af6e2aec552fd2d992200a17b4877df4b95c
MariaPaschevska <maria.paschevska@gmail.com>
2019-02-19 12:21:38 +0200

    uploader updated with progress events

--- src/app/shared/file-uploader/file-uploader.component.ts
+++ src/app/shared/file-uploader/file-uploader.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { HttpClient } from "@angular/common/http";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {HttpClient, HttpEventType} from "@angular/common/http";
 
 @Component({
   selector: 'app-file-uploader',
@@ -23,12 +23,29 @@ export class FileUploaderComponent implements OnInit {
     console.log('Selected file is', this.selectedFile);
   }
 
+  // onUpload() {
+  //   const formData = new FormData();
+  //   formData.append('image', this.selectedFile, this.selectedFile.name);
+  //   this.http.post(this.url, formData)
+  //     .subscribe(response => {
+  //       console.log('onUpload file response', response);
+  //     });
+  //
+  // }
+
   onUpload() {
     const formData = new FormData();
     formData.append('image', this.selectedFile, this.selectedFile.name);
-    this.http.post(this.url, formData)
-      .subscribe(response => {
-        console.log('onUpload file response', response);
+    this.http.post(this.url, formData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          console.log('onUpload file progress', Math.round(event.loaded / event.total * 100) + '%');
+        } else if (event.type === HttpEventType.Response) {
+          console.log('onUpload file response', event);
+        }
       });
 
   }
